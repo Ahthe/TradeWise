0a7b0fc2df0bf754f5dc2a7daf11181582ed8e18
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-08-11 17:27:18 -0400

    changed the name which I kept it the new name

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -5,7 +5,7 @@ import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
 
 export const metadata = {
-  title: 'StockHelper - AI Chatbot'
+  title: 'TradeWise - AI Chatbot'
 }
 
 export default async function IndexPage() {
